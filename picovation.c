519c865c15ae92a9fe763bb0401b8287fbc4d8ed
denybear <98181634+denybear@users.noreply.github.com>
2023-05-10 18:34:54 +0200

    Tap tempo fct in progress

--- picovation.c
+++ picovation.c
@@ -44,11 +44,11 @@
 #include "tusb.h"
 
 // constants
-#define MIDI_CLOCK  0xF8
-#define MIDI_PLAY 0xFA
-#define MIDI_STOP 0xFC
-#define MIDI_PAUSE 0xFB
-#define MIDI_PRG_CHANGE 0xCF	// 0xC0 is program change, 0x0F is midi channel
+#define MIDI_CLOCK		0xF8
+#define MIDI_PLAY		0xFA
+#define MIDI_STOP		0xFC
+#define MIDI_PAUSE		0xFB
+#define MIDI_PRG_CHANGE	0xCF	// 0xC0 is program change, 0x0F is midi channel
 
 #define LED_GPIO	25	// onboard led
 #define LED2_GPIO	255	// 2nd led
@@ -63,15 +63,20 @@ const uint NO_LED2_GPIO = 255;
 
 #define SWITCH_PREV		11		// previous session
 #define SWITCH_NEXT		15		// next session
-#define SWITCH_PLAY		12		// play
-#define SWITCH_PAUSE	14		// pause
+#define SWITCH_PLAY		14		// play
+#define SWITCH_PAUSE	12		// pause
+#define SWITCH_TEMPO	13		// tap tempo
 #define PREV			1
 #define NEXT			2
 #define PLAY			4
 #define PAUSE			8
+#define TEMPO			16
 
 #define FALSE			0
 #define TRUE 			1
+
+#define EXIT_FUNCTION	3000	// 3000 msec = 3 sec
+#define NB_TICKS		24		// 24 ticks per beat (quarter note)
 
 // globals
 static uint8_t song = 0;
@@ -84,109 +89,135 @@ int test_switch (int pedal_to_check)
 	int result = 0;
 	
 	
-    // test if switch has been pressed
-    // in this case, line is down (level 0)
-    if ((pedal_to_check & PREV) && gpio_get (SWITCH_PREV)==0) {
-		  result |= PREV;
-    }
+	// test if switch has been pressed
+	// in this case, line is down (level 0)
+	if ((pedal_to_check & PREV) && gpio_get (SWITCH_PREV)==0) {
+		result |= PREV;
+	}
 
-    if ((pedal_to_check & NEXT) && gpio_get (SWITCH_NEXT)==0) {
-		  result |= NEXT;
-    }
+	if ((pedal_to_check & NEXT) && gpio_get (SWITCH_NEXT)==0) {
+		result |= NEXT;
+	}
 
-    if ((pedal_to_check & PLAY) && gpio_get (SWITCH_PLAY)==0) {
-		  result |= PLAY;
-    }
+	if ((pedal_to_check & PLAY) && gpio_get (SWITCH_PLAY)==0) {
+		result |= PLAY;
+	}
 
-    if ((pedal_to_check & PAUSE) && gpio_get (SWITCH_PAUSE)==0) {
-		  result |= PAUSE;
-    }
+	if ((pedal_to_check & PAUSE) && gpio_get (SWITCH_PAUSE)==0) {
+		result |= PAUSE;
+	}
+
+	if ((pedal_to_check & TEMPO) && gpio_get (SWITCH_TEMPO)==0) {
+		result |= TEMPO;
+	}
 
 	// LED ON if a switch has been pressed
 	if (result) {
-    if (NO_LED_GPIO != LED_GPIO) gpio_put(LED_GPIO, true);		// if onboard led and if we are within time window, lite LED on
-    if (NO_LED2_GPIO != LED2_GPIO) gpio_put(LED2_GPIO, true);	// if another led and if we are within time window, lite LED on
-    // anti-bounce of 30ms
+		if (NO_LED_GPIO != LED_GPIO) gpio_put(LED_GPIO, true);		// if onboard led and if we are within time window, lite LED on
+		if (NO_LED2_GPIO != LED2_GPIO) gpio_put(LED2_GPIO, true);	// if another led and if we are within time window, lite LED on
+		// anti-bounce of 30ms
 		sleep_ms (30);
 		return result;
 	}
 
-  // no switch pressed : LED off
-  if (NO_LED_GPIO != LED_GPIO) gpio_put(LED_GPIO, false);		// if onboard led and if we are within time window, lite LED on
-  if (NO_LED2_GPIO != LED2_GPIO) gpio_put(LED2_GPIO, false);	// if another led and if we are within time window, lite LED on
-  return FALSE;
+	// no switch pressed : LED off
+	if (NO_LED_GPIO != LED_GPIO) gpio_put(LED_GPIO, false);		// if onboard led and if we are within time window, lite LED on
+	if (NO_LED2_GPIO != LED2_GPIO) gpio_put(LED2_GPIO, false);	// if another led and if we are within time window, lite LED on
+	return FALSE;
 }
 
 
 // write lg bytes stored in buffer to midi out
 static void send_midi (bool connected, uint8_t * buffer, uint32_t lg)
 {
-    uint32_t nwritten;
+	uint32_t nwritten;
 
-    if (connected && tuh_midih_get_num_tx_cables(midi_dev_addr) >= 1)
-    {
-        nwritten = tuh_midi_stream_write(midi_dev_addr, 0, buffer, lg);
-        if (nwritten != lg) {
-            TU_LOG1("Warning: Dropped %ld byte\r\n", (lg-nwritten));
-        }
-    }
+	if (connected && tuh_midih_get_num_tx_cables(midi_dev_addr) >= 1)
+	{
+		nwritten = tuh_midi_stream_write(midi_dev_addr, 0, buffer, lg);
+		if (nwritten != lg) {
+			TU_LOG1("Warning: Dropped %ld byte\r\n", (lg-nwritten));
+		}
+	}
 }
 
 
+// poll USB receive and process received bytes accordingly
 static void poll_usb_rx(bool connected)
 {
-    // device must be attached and have at least one endpoint ready to receive a message
-    if (!connected || tuh_midih_get_num_rx_cables(midi_dev_addr) < 1)
-    {
-        return;
-    }
-    tuh_midi_read_poll(midi_dev_addr);
+	// device must be attached and have at least one endpoint ready to receive a message
+	if (!connected || tuh_midih_get_num_rx_cables(midi_dev_addr) < 1)
+	{
+		return;
+	}
+	tuh_midi_read_poll(midi_dev_addr);
+}
+
+
+// called at regular time (alert); this is used to send MIDI clock signal at regular intervals (tap tempo functionality) 
+// shall return >0 to reschedule the same alarm this many us from the time this method returns
+int64_t alarm_callback (alarm_id_t id, void *user_data)
+{
+
+// HERE: SEND MIDI CLOCK
+	printf("Timer %d fired!\n", (int) id);
+	timer_fired = true;
+	// Can return a value here in us to fire in the future
+	return 0;
 }
 
 
 int main() {
-    
-  bool connected;
+	
+	bool connected;
 	int pedal;
 	uint8_t data [3];	// midi data to send
-	
-  board_init();
-  printf("Picovation\r\n");
-  tusb_init();
 
-  // Map the pins to functions
-  gpio_init(LED_GPIO);
-  gpio_set_dir(LED_GPIO, GPIO_OUT);
-
-  gpio_init(LED2_GPIO);
-  gpio_set_dir(LED2_GPIO, GPIO_OUT);
-
-  gpio_init(SWITCH_PREV);
-  gpio_set_dir(SWITCH_PREV, GPIO_IN);
-  gpio_pull_up (SWITCH_PREV);       // switch pull-up
-
-  gpio_init(SWITCH_NEXT);
-  gpio_set_dir(SWITCH_NEXT, GPIO_IN);
-  gpio_pull_up (SWITCH_NEXT);       // switch pull-up
-
-  gpio_init(SWITCH_PLAY);
-  gpio_set_dir(SWITCH_PLAY, GPIO_IN);
-  gpio_pull_up (SWITCH_PLAY);       // switch pull-up
-
-  gpio_init(SWITCH_PAUSE);
-  gpio_set_dir(SWITCH_PAUSE, GPIO_IN);
-  gpio_pull_up (SWITCH_PAUSE);       // switch pull-up
+	bool tempo = false;					// indicates whether tempo functionality is on or off
+	uint32_t press_on, press_off;		// time for tap tempo function, to enable / disable tap tempo functionality
+	uint32_t this_press, previous_press;		// time for tap tempo function, to measure timing between 1st and 2nd press
+	alarm_id_t alarm_id;						// id of alarm used to send MIDI clock at regular intervals
 
 
-  // main loop
-  while (1) {
+	stdio_init_all();
+	board_init();
+	printf("Picovation\r\n");
+	tusb_init();
 
-    tuh_task();
-    // check connection to USB slave
-    connected = midi_dev_addr != 0 && tuh_midi_configured(midi_dev_addr);
+	// Map the pins to functions
+	gpio_init(LED_GPIO);
+	gpio_set_dir(LED_GPIO, GPIO_OUT);
 
-	  // test pedal and check if one of them is pressed
-    pedal = test_switch (PREV | NEXT | PLAY | PAUSE);
+	gpio_init(LED2_GPIO);
+	gpio_set_dir(LED2_GPIO, GPIO_OUT);
+
+	gpio_init(SWITCH_PREV);
+	gpio_set_dir(SWITCH_PREV, GPIO_IN);
+	gpio_pull_up (SWITCH_PREV);		 // switch pull-up
+
+	gpio_init(SWITCH_NEXT);
+	gpio_set_dir(SWITCH_NEXT, GPIO_IN);
+	gpio_pull_up (SWITCH_NEXT);		 // switch pull-up
+
+	gpio_init(SWITCH_PLAY);
+	gpio_set_dir(SWITCH_PLAY, GPIO_IN);
+	gpio_pull_up (SWITCH_PLAY);		 // switch pull-up
+
+	gpio_init(SWITCH_PAUSE);
+	gpio_set_dir(SWITCH_PAUSE, GPIO_IN);
+	gpio_pull_up (SWITCH_PAUSE);		 // switch pull-up
+
+
+	// main loop
+	while (1) {
+
+		tuh_task();
+		// check connection to USB slave
+		connected = midi_dev_addr != 0 && tuh_midi_configured(midi_dev_addr);
+
+		// test pedal and check if one of them is pressed
+		pedal = test_switch (PREV | NEXT | PLAY | PAUSE | TEMPO);
+
 
 		if (pedal & (NEXT | PREV)) {
 			// previous or next session
@@ -198,6 +229,7 @@ int main() {
 			data [1] = song; 
 			send_midi (connected, data, 2);
 		}
+
 
 		if (pedal & PLAY) {
 			// play / stop
@@ -212,28 +244,83 @@ int main() {
 			send_midi (connected, data, 1);
 		}
 
+
 		if (pedal & PAUSE) {
 			// pause
 			data [0] = MIDI_PAUSE;
 			send_midi (connected, data, 1);
 		}
 
+
+		if (pedal & TEMPO) {
+			// Tap tempo functionality
+			
+			// get current time
+			this_press = to_ms_since_boot (get_absolute_time());
+			press_on = this_press;
+
+			// In case this is the first time we press the tempo pedal
+			if (!tempo) {
+				// set key variables and do nothing
+				tempo = true;
+				previous_press = this_press;
+			}
+			// this is not the first time we press the tempo pedal
+			else {
+// HERE 
+				// calculate time difference
+				// in case time difference is too short, do not take into account: do not change alarms, and consider this is the first press of pedal
+				// in case time difference is too large, do not take into account: do not change alarms, and consider this is the first press of pedal
+					// else
+					// calculate interval between MIDI ticks
+					// set alarm to send MIDI clock for each MIDI tick
+
+// HERE: SET ALARM
+// Call alarm_callback in 2 seconds
+alarm_id = add_alarm_in_ms(2000, alarm_callback, NULL, false);
+
+
+
+				// in any case, current time (time of this press) becomes time of previous press, in order to prepare for next press
+				previous_press = this_press;
+			}
+
+			// wait for pedal to be unpressed; during that time, make sure we receive incoming midi events though
+			while (test_switch (PREV | NEXT | PLAY | PAUSE | TEMPO)) {
+				// read MIDI events coming from groovebox and manage accordingly
+				// commented as this may not be necessary
+				// poll_usb_rx (connected);
+			}
+
+			// if the pedal has been pressed for 3+ seconds, then disable tap tempo functionality
+			// get current time
+			press_off = to_ms_since_boot (get_absolute_time());
+			// check whether pedal has been pressed for 3+ seconds in which case we disable the function
+			if (press_off - press_on > EXIT_FUNCTION) {
+				// remove alarm
+				cancel_alarm (alarm_id);
+				// set functionality off
+				tempo = false;
+			}
+		}
+
+
 		if (pedal) {
 			// if pedal is pressed, then flush send buffer
 			if (connected) tuh_midi_stream_flush(midi_dev_addr);
 			// wait for pedal to be unpressed; during that time, make sure we receive incoming midi events though
-			while (test_switch (PREV | NEXT | PLAY | PAUSE)) {
-    		// read MIDI events coming from groovebox and manage accordingly
-        // commented as this may not be necessary
-        // poll_usb_rx (connected);
-      }
+			while (test_switch (PREV | NEXT | PLAY | PAUSE | TEMPO)) {
+				// read MIDI events coming from groovebox and manage accordingly
+				// commented as this may not be necessary
+				// poll_usb_rx (connected);
+			}
 		}
 		
 		// flush outgoing data, and read MIDI events coming from groovebox and manage accordingly
 		if (connected) tuh_midi_stream_flush(midi_dev_addr);
 		poll_usb_rx (connected);
 		
-  }
+	}
 }
 
 
@@ -248,66 +335,66 @@ int main() {
 // therefore report_desc = NULL, desc_len = 0
 void tuh_midi_mount_cb(uint8_t dev_addr, uint8_t in_ep, uint8_t out_ep, uint8_t num_cables_rx, uint16_t num_cables_tx)
 {
-  printf("MIDI device address = %u, IN endpoint %u has %u cables, OUT endpoint %u has %u cables\r\n",
-      dev_addr, in_ep & 0xf, num_cables_rx, out_ep & 0xf, num_cables_tx);
+	printf("MIDI device address = %u, IN endpoint %u has %u cables, OUT endpoint %u has %u cables\r\n",
+		dev_addr, in_ep & 0xf, num_cables_rx, out_ep & 0xf, num_cables_tx);
 
-  if (midi_dev_addr == 0) {
-    // then no MIDI device is currently connected
-    midi_dev_addr = dev_addr;
-  }
+	if (midi_dev_addr == 0) {
+		// then no MIDI device is currently connected
+		midi_dev_addr = dev_addr;
+	}
 
-  else {
-    printf("A different USB MIDI Device is already connected.\r\nOnly one device at a time is supported in this program\r\nDevice is disabled\r\n");
-  }
+	else {
+		printf("A different USB MIDI Device is already connected.\r\nOnly one device at a time is supported in this program\r\nDevice is disabled\r\n");
+	}
 }
 
 // Invoked when device with hid interface is un-mounted
 void tuh_midi_umount_cb(uint8_t dev_addr, uint8_t instance)
 {
-  if (dev_addr == midi_dev_addr) {
-    midi_dev_addr = 0;
-    printf("MIDI device address = %d, instance = %d is unmounted\r\n", dev_addr, instance);
-  }
-  else {
-    printf("Unused MIDI device address = %d, instance = %d is unmounted\r\n", dev_addr, instance);
-  }
+	if (dev_addr == midi_dev_addr) {
+		midi_dev_addr = 0;
+		printf("MIDI device address = %d, instance = %d is unmounted\r\n", dev_addr, instance);
+	}
+	else {
+		printf("Unused MIDI device address = %d, instance = %d is unmounted\r\n", dev_addr, instance);
+	}
 }
 
 // invoked when receiving some MIDI data
 void tuh_midi_rx_cb(uint8_t dev_addr, uint32_t num_packets)
 {
-    if (midi_dev_addr == dev_addr)
-    {
-        if (num_packets != 0)
-        {
-            uint8_t cable_num;
-            uint8_t buffer[48];   // 48 should be enough as we only get Program Change data
-            while (1) {
-                uint32_t bytes_read = tuh_midi_stream_read(dev_addr, &cable_num, buffer, sizeof(buffer));
-                if (bytes_read == 0) return;
-                if (cable_num == 0) {
-					        // test values received from groovebox via MIDI
-                  // this is a poor way of testing; in some cases, we receive much more than 1 or 2 bytes
-                  // and incoming MIDI message should be parsed
-                  // in the case we only receive Program Change data, this testing should be sufficient
-					        if (bytes_read == 1) {		// test MIDI PLAY or STOP
-						        if (buffer [0] == MIDI_PLAY) play = true;
-						        if (buffer [0] == MIDI_STOP) play = false;
-					        }
-					      if (bytes_read == 2) {		// test MIDI PROGRAM CHANGE
-						      if (buffer [0] == MIDI_PRG_CHANGE)
-							      if (buffer [1] <= 31) song = buffer [1];		// make sure song number is inside boudaries (0 to 31)
-					      }
-                }
-            }
-        }
-    }
+	if (midi_dev_addr == dev_addr)
+	{
+		if (num_packets != 0)
+		{
+			uint8_t cable_num;
+			uint8_t buffer[48];   // 48 should be enough as we only get Program Change data
+			while (1) {
+				uint32_t bytes_read = tuh_midi_stream_read(dev_addr, &cable_num, buffer, sizeof(buffer));
+				if (bytes_read == 0) return;
+				if (cable_num == 0) {
+							// test values received from groovebox via MIDI
+				  // this is a poor way of testing; in some cases, we receive much more than 1 or 2 bytes
+				  // and incoming MIDI message should be parsed
+				  // in the case we only receive Program Change data, this testing should be sufficient
+							if (bytes_read == 1) {		// test MIDI PLAY or STOP
+								if (buffer [0] == MIDI_PLAY) play = true;
+								if (buffer [0] == MIDI_STOP) play = false;
+							}
+						  if (bytes_read == 2) {		// test MIDI PROGRAM CHANGE
+							  if (buffer [0] == MIDI_PRG_CHANGE)
+								  if (buffer [1] <= 31) song = buffer [1];		// make sure song number is inside boudaries (0 to 31)
+						  }
+				}
+			}
+		}
+	}
 
-    return;
+	return;
 }
 
 // invoked when sending some MIDI data
 void tuh_midi_tx_cb(uint8_t dev_addr)
 {
-    (void)dev_addr;
+	(void)dev_addr;
 }
